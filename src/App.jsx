e0fa63ad427cfdd86e8ab9ab5c0dcd17bdcb82e8
Muhammad Shoaib <94863313+shoaib7619@users.noreply.github.com>
2024-03-18 11:36:40 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
+import './App.css';
 import React from 'react';
-import questions from './data/question.json'
+import questions from './data/question.json';
 import Header from './components/header';
-import Footer from './components/footer';
-
 
 function App() {
   const [allQuestions, setAllQuestions] = React.useState(questions);
@@ -13,84 +11,75 @@ function App() {
   const q = allQuestions[index];
   const imgSrc = `../img/${q.img}`;
 
-  const onSelectOption = (qimg,op) => {
+  const onSelectOption = (qimg, op) => {
     const copyQuestions = [...allQuestions];
-    const question = copyQuestions.find(q => q.img === qimg);
+    const question = copyQuestions.find((q) => q.img === qimg);
     question.selectedOption = op;
     setAllQuestions(copyQuestions);
-    setClickCount(clickCount+1)
-  }
+    setClickCount(clickCount + 1);
+  };
 
+  const getOptionStyle = (q, op) => {
+    const style = 'list-group-item';
+    if (op !== q.selectedOption) return style;
+    if (op === q.correctOption) return style + ' bg-success';
+    else return style + ' bg-danger';
+  };
 
-   const getOptionStyle = (q, op) => {
-    const style = "list-group-item";
-     if(op !== q.selectedOption)
-         return style;
-     // you are rendering selected option
-     if(op === q.correctOption)
-      return style + " bg-success";
-     else
-       return style + " bg-danger";
-   }
-
-
-   const calculateScore = () => {
+  const calculateScore = () => {
     const correctAnswers = allQuestions.filter((q) => q.selectedOption === q.correctOption);
     return correctAnswers.length;
   };
-
 
   const calculatePercentage = () => {
     const attemptedQuestions = allQuestions.filter((q) => q.selectedOption !== undefined);
     return (attemptedQuestions.length / allQuestions.length) * 100;
   };
 
-
   return (
     <>
-  
-     <div className="container">
-        <Header/>
-     <div className="text-center bg-white">
-          <h3>Question {index+1}/{allQuestions.length}</h3>
-          </div>
-         <div className="card my-2" key={q.id}>
-         <div className="text-center">
-          <img src={imgSrc} alt="" height="200px" width="400px" />
+      <div className="container">
+        <Header />
+        <div className="text-center bg-white">
+          <h3>
+            Question {index + 1}/{allQuestions.length}
+          </h3>
         </div>
-           <ul className="list-group list-group-flush">
-             {q.options.map((op) => (
+        <div className="card my-2" key={q.id}>
+          <div className="text-center">
+            <img src={imgSrc} alt="" className="img-fluid img-mobile-width-230" />
+          </div>
+          <ul className="list-group list-group-flush">
+            {q.options.map((op) => (
               <li
-                 key={op}
-                className={getOptionStyle(q, op)}
+                key={op}
+                className={`${getOptionStyle(q, op)} custom-list-item`}
                 onClick={() => onSelectOption(q.img, op)}
               >
-                 {op}
-               </li>
+                {op}
+              </li>
             ))}
           </ul>
-         </div>
-         <button 
-                className="btn btn-primary m-3" 
-                onClick={() => setIndex(index - 1)}
-                disabled={index === 0}>Pervious</button>
+        </div>
+        <button className="btn btn-primary m-3" onClick={() => setIndex(index - 1)} disabled={index === 0}>
+          Previous
+        </button>
+        <button
+          className="btn btn-primary m-3"
+          onClick={() => setIndex(index + 1)}
+          disabled={index === allQuestions.length - 1}
+        >
+          Next
+        </button>
 
-                <button 
-                className="btn btn-primary m-3" 
-                onClick={() => setIndex(index + 1)}
-                disabled={index === allQuestions.length - 1}>Next</button>
-
- 
-     <div className="d-flex justify-content-around ">
-            <h4 >Total Clicks: {clickCount}</h4>
-            <h4>Percentage Attempted: {calculatePercentage().toFixed(2)}%</h4>
-            <h4>Your Score: {calculateScore()}</h4>
-            </div>
-
-       <Footer/> 
+        <div className="d-flex justify-content-around">
+          <h4>Total Clicks: {clickCount}</h4>
+          <h4>Percentage Attempted: {calculatePercentage().toFixed(2)}%</h4>
+          <h4>Your Score: {calculateScore()}</h4>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
